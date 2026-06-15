7184a78697d3fb501894d3016df0c9b7ae9e0be5
JoeCheong <95924085+Joecheong2006@users.noreply.github.com>
2026-06-15 00:21:29 -0700

    Add: minimum-window-with-characters - submission-27

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-27.py
@@ -0,0 +1,30 @@
+class Solution:
+    def map_contained(self, m1, m2):
+        for key, value in m1.items():
+            if (not key in m2) or (m2[key] < value):
+                return False
+        return True
+
+    def minWindow(self, s: str, t: str) -> str:
+        counts = defaultdict(int)
+        tcounts = defaultdict(int)
+
+        l = 0
+        res = 10000
+
+        ress = ""
+
+        for c in t:
+            tcounts[c] += 1
+
+        for r in range(len(s)):
+            counts[s[r]] += 1
+
+            while self.map_contained(tcounts, counts):
+                if r - l + 1 < res:
+                    res = r - l + 1
+                    ress = s[l:(l+res)]
+                counts[s[l]] -= 1
+                l += 1
+
+        return ress
