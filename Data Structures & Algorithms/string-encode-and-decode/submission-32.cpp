8f23cca2a44c3286b866316d7d12711afdf654f1
JoeCheong <95924085+Joecheong2006@users.noreply.github.com>
2026-08-25 23:02:26 +0800

    Add: string-encode-and-decode - submission-32

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-32.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        string s;
+        for (const auto &str : strs) {
+            s.push_back(static_cast<char>(static_cast<int>(str.size()) - 100));
+            cout << str.size() << ' ';
+            s += str;
+        }
+        return s;
+    }
+
+    vector<string> decode(string s) {
+        std::size_t curr = 0, l = s.size();
+        vector<string> strs;
+
+        while (curr < l) {
+            int str_len = static_cast<int>(s[curr]) + 100;
+            auto str = s.substr(curr + 1, str_len);
+            strs.push_back(std::move(str));
+            curr += str_len + 1;
+        }
+
+        return strs;
+    }
+};
