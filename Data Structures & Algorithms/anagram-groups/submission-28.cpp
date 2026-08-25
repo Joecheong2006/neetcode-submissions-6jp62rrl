9cfbfcfdc6c60d7d63aad5e5fd16e83df27db2d0
JoeCheong <95924085+Joecheong2006@users.noreply.github.com>
2026-08-25 21:35:23 +0800

    Add: anagram-groups - submission-28

---
+++ Data Structures & Algorithms/anagram-groups/submission-28.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        std::unordered_map<std::string, std::vector<string>> m;
+
+        for (const auto &s : strs) {
+            string count;
+            count.resize(26);
+            for (const auto &c : s) {
+                count[c - 'a'] += 1;
+            }
+            m[count].push_back(s);
+        }
+
+        std::vector<std::vector<string>> res;
+        res.reserve(m.size());
+
+        for (const auto &p : m) {
+            res.push_back(p.second);
+        }
+
+        return res;
+    }
+};
