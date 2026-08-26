826b80bd7d921ebe23ba5a35a3f8993cf43a9c9c
JoeCheong <95924085+Joecheong2006@users.noreply.github.com>
2026-08-26 21:12:05 +0800

    Add: valid-sudoku - submission-7

---
+++ Data Structures & Algorithms/valid-sudoku/submission-7.py
@@ -0,0 +1,38 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        nums = [0] * 90
+
+        for i in range(9):
+            for j in range(9):
+                if board[i][j] == ".":
+                    continue
+                index = ord(board[i][j][0]) - ord('1')
+                if nums[index] == 1:
+                    return False
+                nums[index] = 1
+            nums = [0] * 9
+
+        for i in range(9):
+            for j in range(9):
+                if board[j][i] == ".":
+                    continue
+                index = ord(board[j][i][0]) - ord('1')
+                if nums[index] == 1:
+                    return False
+                nums[index] = 1
+            nums = [0] * 9
+
+        for bi in range(0, 9, 3):
+            for bj in range(0, 9, 3):
+                for i in range(bi, bi + 3):
+                    for j in range(bj, bj + 3):
+                        if board[i][j] == ".":
+                            continue
+                        index = ord(board[i][j][0]) - ord('1')
+                        if nums[index] == 1:
+                            return False
+                        nums[index] = 1
+                nums = [0] * 9
+
+        return True
+        
