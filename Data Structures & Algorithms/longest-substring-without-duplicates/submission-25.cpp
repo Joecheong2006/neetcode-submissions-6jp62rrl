c24fbdaf9b2d1ace99aac2dbb12ca314034e639d
JoeCheong <95924085+Joecheong2006@users.noreply.github.com>
2026-09-25 21:46:47 -0700

    Add: longest-substring-without-duplicates - submission-25

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-25.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int l = 0, maxLen = 0;
+        std::unordered_map<int, int> mp;
+
+        for (int r = 0; r < s.size(); ++r) {
+            if (mp.find(s[r]) != mp.end() && mp[s[r]] >= l) {
+                l = mp[s[r]] + 1;
+            }
+            mp[s[r]] = r;
+            maxLen = max(maxLen, r - l + 1);
+        }
+
+        return maxLen;
+    }
+};
